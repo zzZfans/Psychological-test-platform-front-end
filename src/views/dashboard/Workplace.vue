c8e156fe07cdc42fb67322b0a41a42664f3d00fa
li-song <1515581056@qq.com>
2022-05-14 11:03:18 +0800

    轮播及动态用户信息

--- src/views/dashboard/Workplace.vue
+++ src/views/dashboard/Workplace.vue
@@ -1,6 +1,6 @@
 <template>
   <page-header-wrapper :title="'首页'">
-    <template v- v-slot:content>
+    <template  v-slot:content>
       <div class="page-header-content">
         <div class="avatar">
           <a-avatar size="large" :src="currentUser.avatar" />
@@ -9,16 +9,16 @@
           <div class="content-title">
             {{ timeFix }}，{{ user.name }}<span class="welcome-text">，{{ welcome }}</span>
           </div>
+          <!--          <div>前端工程师 | 蚂蚁金服 - 某某某事业群 - VUE平台</div>-->
         </div>
       </div>
-    </template>
-
+    </template>>
     <div>
       <a-row :span="24" >
         <a-col >
           <a-card
             class="project-list"
-            :loading="loading"
+            :loading="false"
             style="margin-bottom: 24px;"
             :bordered="false"
             title="热门"
@@ -28,38 +28,61 @@
             <div>
             </div>
           </a-card>
-<!--          轮播-->
-          <template>
-            <div class="modal_box" style="width: 100%;height: 500px;background-color: #99a9bf">
-              <a-icon type="left" style="font-size: 50px" @click="handlePrev" />
-              <a-carousel autoplay ref="img" >
-                <a href="https://www.baidu.com"  target="_blank">
-                  <img src="https://gw.alipayobjects.com/zos/antfincdn/XAosXuNZyF/BiazfanxmamNRoxxVxka.png">
-                </a>
-              </a-carousel>
-              <a-icon
-                type="right"
-                style="font-size: 50px;float: right"
-                @click="handleNext"
-              />
+          <!--          轮播-->
+          <div class="showImg" style="position:relative;height: 400px;width: 1100px;background-color: red" >
+<!--            //轮播图片-->
+            <a href="https://www.baidu.com"  target="_blank">
+            <img
+              @mouseover="changeInterval(true)"
+              @mouseleave="changeInterval(false)"
+              v-for="(item) in imgArr"
+              :key="item.id"
+              :src="item.url"
+              alt="暂无图片"
+              v-show="item.id===currentIndex"
+            >
+            </a>
+<!--            //左侧按钮-->
+            <div @click="clickIcon('up')" class="iconDiv icon-left">
+              <i class="el-icon-caret-left"></i>
             </div>
-          </template>
-<!--          轮播-->
+<!--            //右侧按钮-->
+            <div @click="clickIcon('down')" class="iconDiv icon-right">
+              <i class="el-icon-caret-right"></i>
+            </div>
+<!--            //控制圆点-->
+            <div class="banner-circle">
+              <ul>
+                <li
+                  @click="changeImg(item.id)"
+                  v-for="(item) in imgArr"
+                  :key="item.id"
+                  :class="item.id===currentIndex? 'active': '' "
+                ></li>
+              </ul>
+            </div>
+          </div>
+          <!--          轮播-->
         </a-col>
       </a-row>
+          <!--      动态-->
       <a-row >
-        <a-col :xl="10" :lg="24" :md="24" :sm="24" :xs="24">
-          <div :span="12">
-            <a-card :loading="loading" title="动态" :bordered="false">
-              <a-list>
-                <a-list-item :key="index" v-for="(item, index) in activities">
-                  <a-list-item-meta>
-                    <a-avatar slot="avatar" size="small" :src="avatar" />
-                    <div slot="title">
-                      <span>{{ item.user.nickname }}</span>&nbsp; 在&nbsp;<a href="#">{{ item.project.name }}</a>&nbsp;
-                      <span>{{ item.project.action }}</span>&nbsp;
-                      <a href="#">{{ item.project.event }}</a>
-                    </div>
+        <a-col :xl="13" :lg="24" :md="24" :sm="24" :xs="24">
+          <div >
+            <a-card :loading="false" title="动态" :bordered="false">
+              <a-list
+                :data-source="data"
+              >
+                <a-list-item slot="renderItem" slot-scope="item">
+                  <a-list-item-meta >
+                    <!--                    <a-avatar slot="avatar" size="small" :src="item.avatar" />-->
+                    <a slot="title" >
+                      <span style="color: #1890ff">{{ item.username }}</span>&nbsp; 在&nbsp;<a href="#">{{ item.createTime }}</a>&nbsp;
+                      <span>测试了</span>&nbsp;
+                      <span style="color: #1890ff">{{ item.assessType }}</span>&nbsp;
+                      <!--                      <a href="#">{{ item.project.event }}</a>-->
+                      <span>...</span>
+                    </a>
                     <div slot="description">{{ item.time }}</div>
                   </a-list-item-meta>
                 </a-list-item>
@@ -71,7 +94,7 @@
         </a-col>
         <a-col
           style="padding: 0 12px"
-          :xl="14"
+          :xl="11"
           :lg="24"
           :md="24"
           :sm="24"
@@ -83,19 +106,17 @@
             :bordered="false"
             :body-style="{ padding: 0 }"
           >
+            <!--                    评论区-->
             <div style="position: relative;top: 10px">
               <a-row>
                 <a-col span="3">
                   <a-avatar size="large" :src="currentUser.avatar" />
                 </a-col>
                 <a-col span="18">
-                  <a-input style="position: relative;height: 40px;float: left;width: 230px" placeholder="输入评论"></a-input>
+                  <a-input style="position: relative;height: 40px;float: left;width: 390px" placeholder="输入评论"></a-input>
                 </a-col>
-                <!--                <a-col span="3">-->
-                <!--                  <a-icon><plus-circle-outlined /></a-icon>-->
-                <!--                </a-col>-->
                 <a-col span="3">
-                  <a-button style="position: relative;height: 40px;float: right" type="primary">发送</a-button>
+                  <a-button style="position: relative;height: 40px;float: right;width: 50px" type="primary">发送</a-button>
                 </a-col>
               </a-row>
               <div>
@@ -105,79 +126,102 @@
                   item-layout="horizontal"
                 >
                   <div v-for="(item) in comments" :key="item.id">
-                    <a-list-item slot="renderItem"  :key="item.id">
-                      <a-comment>
-                        <div style="font-size: small">
-                          <a-avatar
-                            :src="item.avatar"
-                          />
-                          &nbsp
-                          <a>{{ item.userName }}</a>
+                    <a-list-item slot="renderItem" :key="item.id">
+                      <div style="position: relative;background-color: #d3dce6;width: 100%">
+                        <a-comment>
+                          <div style="font-size: small">
+                            <a-avatar
+                              :src="item.avatar"
+                            />
+                            &nbsp
+                            <a>{{ item.userName }}</a>
+                            <a-tooltip>
+                              <span> 发表于 {{ item.createTime }}</span>
+                            </a-tooltip>
+                          </div>
+                          <p style="margin-left: 50px">
+                            {{ item.content }}
+                          </p>
                           <a-tooltip>
-                            <span> 发表于 {{ item.createTime }}</span>
+                            <a-tooltip>
+                              <template slot="title">
+                                评论
+                              </template>
+                              <a-icon
+                                style="position: relative;margin-left: 8%;color: #1976d2"
+                                type="edit"
+                                @click="addComment(item)"
+                              >
+                              </a-icon>
+                            </a-tooltip>
+                            <a v-if="true">
+                              <a-icon
+                                style="position: relative;margin-left: 10%"
+                                type="delete"
+                                color="#1976d2"
+                                @click="deleteComment(item)"
+                              >
+                              </a-icon>
+                            </a>
+                            <a-button @click="getChildren(index,item.id())">
+                              详情
+                            </a-button>
                           </a-tooltip>
+                        </a-comment>
+                        <div v-show="true" style="position: relative;left: 100px;width: 400px;">
+                          <a-comment>
+                            <div style="font-size: small">
+                              <a-avatar
+                                :src="item.avatar"
+                              />
+                              &nbsp
+                              <a>{{ item.userName }}</a>
+                              <a-tooltip>
+                                <span> 发表于 {{ item.createTime }}</span>
+                              </a-tooltip>
+                            </div>
+                            <p style="margin-left: 50px">
+                              {{ item.content }}
+                            </p>
+                            <a-tooltip>
+                              <a-tooltip>
+                                <template slot="title">
+                                  评论
+                                </template>
+                                <a-icon
+                                  style="position: relative;margin-left: 8%;color: #1976d2"
+                                  type="edit"
+                                  @click="addComment(item)"
+                                >
+                                </a-icon>
+                              </a-tooltip>
+                              <a v-if="true">
+                                <a-icon
+                                  style="position: relative;margin-left: 10%"
+                                  type="delete"
+                                  color="#1976d2"
+                                  @click="deleteComment(item)"
+                                >
+                                </a-icon>
+                              </a>
+                            </a-tooltip>
+                          </a-comment>
                         </div>
-                        <p style="margin-left: 50px">
-                          {{ item.content }}
-                        </p>
-                        <a-tooltip>
-                          <a-tooltip>
-                            <template slot="title">
-                              评论
-                            </template>
-                            <a-icon
-                              style="position: relative;margin-left: 8%;color: #1976d2"
-                              type="edit"
-                              @click="addComment(item)"
-                            >
-                            </a-icon>
-                          </a-tooltip>
-                          <a v-if="true">
-                            <a-icon
-                              style="position: relative;margin-left: 10%"
-                              type="delete"
-                              color="#1976d2"
-                              @click="deleteComment(item)"
-                            >
-                            </a-icon>
-                          </a>
-<!--                          <a-button @click="getChildren(index,item.id())">-->
-<!--                            详情-->
-<!--                          </a-button>-->
-                        </a-tooltip>
-                      </a-comment>
+                      </div>
                     </a-list-item>
-<!--                    <a-list-->
-<!--                      v-if="item.count"-->
-<!--                      style="margin-left: 70px"-->
-<!--                      :data-source="children[index]"-->
-<!--                      item-layout="horizontal"-->
-<!--                    >-->
-<!--                    </a-list>-->
+                    <!--                    <a-list-->
+                    <!--                      v-if="item.count"-->
+                    <!--                      style="margin-left: 70px"-->
+                    <!--                      :data-source="children[index]"-->
+                    <!--                      item-layout="horizontal"-->
+                    <!--                    >-->
+                    <!--                    </a-list>-->
                   </div>
                 </a-list>
-                <a-comment>
-                  <a-avatar
-                    slot="avatar"
-                    src="https://cdn.vuetifyjs.com/images/john.jpg"
-                    alt="匿名用户"
-                  >
-                  </a-avatar>
-                  <div slot="content">
-                    <a-form-item>
-                      <a-textarea :rows="4" :value="value" @change="handleCommentChange">
-                      </a-textarea>
-                    </a-form-item>
-                    <a-form-item>
-                      <a-button html-type="submit" :loading="submitting" type="primary" @click="handleCommentSubmit">
-                        发表评论
-                      </a-button>
-                    </a-form-item>
-                  </div>
-                </a-comment>
               </div>
             </div>
           </a-card>
+          <!--                    评论区-->
         </a-col>
       </a-row>
     </div>
@@ -192,7 +236,7 @@ import { Radar } from '@/components'
 import { getRoleList, getServiceList } from '@/api/manage'
 import { getUser } from '@/api/user'
 import { commentList, childrenList } from '@/api/comment'
-
+import { getUserHistoryList } from '@/api/assess'
 const DataSet = require('@antv/data-set')
 
 export default {
@@ -203,6 +247,31 @@ export default {
   },
   data () {
     return {
+      currentIndex: 0, // 当前所在图片下标
+      timer: null, // 定时轮询
+      imgArr: [
+        {	id: 0,
+          url: 'https://gw.alipayobjects.com/zos/antfincdn/XAosXuNZyF/BiazfanxmamNRoxxVxka.png'
+        },
+        {
+          id: 1,
+          url: 'https://i.picsum.photos/id/1006/3000/2000.jpg?hmac=x83pQQ7LW1UTo8HxBcIWuRIVeN_uCg0cG6keXvNvM8g'
+        },
+        {
+          id: 2,
+          url: 'https://gw.alipayobjects.com/zos/antfincdn/XAosXuNZyF/BiazfanxmamNRoxxVxka.png'
+        },
+        {
+          id: 3,
+          url: 'https://gw.alipayobjects.com/zos/antfincdn/XAosXuNZyF/BiazfanxmamNRoxxVxka.png'
+        }
+      ],
+      // 评论处理部分
+      commentShow: false,
+      plheight: '0px',
+      data: [],
+      childrenList: [],
+      // 评论处理部分
       value: '',
       comments: [],
       children: '',
@@ -292,9 +361,76 @@ export default {
     this.getTeams()
     this.initRadar()
     this.getUser()
+    this.getUserHistoryList()
+    this.startInterval()
   },
   methods: {
+    // 获取子评论
+    // childrenList () {
+    //   const id =
+    //   childrenList(id).then(res => {
+    //     if (res.success){
+    //
+    //     }
+    //   })
+    // },
+    // 开启定时器
+    startInterval () {
+      // 事件里定时器应该先清除在设置，防止多次点击直接生成多个定时器
+      clearInterval(this.timer)
+      this.timer = setInterval(() => {
+        this.currentIndex++
+        if (this.currentIndex > this.imgArr.length - 1) {
+          this.currentIndex = 0
+        }
+      }, 3000)
+    },
+    // 点击左右箭头
+    clickIcon (val) {
+      if (val === 'down') {
+        this.currentIndex++
+        if (this.currentIndex === this.imgArr.length) {
+          this.currentIndex = 0
+        }
+      } else {
+        /* 第一种写法
+        this.currentIndex--;
+        if(this.currentIndex < 0){
+          this.currentIndex = this.imgArr.length-1;
+        } */
+        // 第二种写法
+        if (this.currentIndex === 0) {
+          this.currentIndex = this.imgArr.length
+        }
+        this.currentIndex--
+      }
+    },
+    // 点击控制圆点
+    changeImg (index) {
+      this.currentIndex = index
+    },
+    // 鼠标移入移出控制
+    changeInterval (val) {
+      if (val) {
+        clearInterval(this.timer)
+      } else {
+        this.startInterval()
+      }
+    },
+    getUserHistoryList () {
+      const data = {
+        pageSize: 10
+      }
+      getUserHistoryList(data).then(res => {
+        if (res.success) {
+          this.data = res.result.records
+          // alert(this.data.createTime)
+        }
+      })
+    },
     getChildren (index, id) {
+      // this.commentShow = true
+      alert('莱菲欧')
       childrenList(id).then(res => {
         if (res.success) {
           this.children[index] = res.result
@@ -510,5 +646,75 @@ export default {
 }
 .ant-carousel {
   width: 75%;
+}
+* {
+  padding: 0;
+  margin: 0;
+}
+/* 清除li前面的圆点 */
+li {
+  list-style-type: none;
+}
+.showImg{
+  position: relative;
+  width: 40%;
+  height: 250px;
+  margin: 100px auto;
+  overflow: hidden;
+}
+/* 轮播图片 */
+.showImg img{
+  width: 100%;
+  height: 100%;
+}
+
+/* 箭头图标 */
+.iconDiv{
+  position: absolute;
+  top: 50%;
+  transform: translateY(-50%);
+  width: 30px;
+  height: 30px;
+  border: 1px solid #666;
+  border-radius: 15px;
+  background-color: rgba(125,125,125,.2);
+  line-height: 30px;
+  text-align: center;
+  font-size: 25px;
+  cursor: pointer;
+}
+.iconDiv:hover{
+  background-color: white;
+}
+.icon-left{
+  left: 10px;
+}
+.icon-right{
+  right: 10px;
+}
+
+/* 控制圆点 */
+.banner-circle{
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  height: 20px;
+}
+.banner-circle ul{
+  margin: 0 50px;
+  height: 100%;
+  text-align: right;
+}
+.banner-circle ul li{
+  display: inline-block;
+  width: 14px;
+  height: 14px;
+  margin: 0 5px;
+  border-radius: 7px;
+  background-color: rgba(125,125,125,.8);
+  cursor: pointer;
+}
+.active{
+  background-color: black !important;
 }
 </style>
