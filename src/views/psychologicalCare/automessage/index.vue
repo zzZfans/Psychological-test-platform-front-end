ebda399c6ce1673e06c1fe97780b1df4226eacd9
zhangzhenc <zhangzhencheng1598@163.com>
2022-06-01 23:50:02 +0800

    fix: bug

--- src/views/psychologicalCare/automessage/index.vue
+++ src/views/psychologicalCare/automessage/index.vue
@@ -320,6 +320,7 @@ export default {
       this.visible = false
       this.msgId = ''
       this.form.resetFields()
+      this.html = ''
     },
     afterVisibleChange () {
       this.visible = false
